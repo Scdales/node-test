732f2db8236aea35a1a18c4cdacf4ad6c72c3b40
Sean <seancliffdaley@gmail.com>
2018-04-01 18:51:52 +0100

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 //Local objects
@@ -66,6 +67,33 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({todo});
     }).catch((e) => 
         res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // This creates a subset of the passed object, preventing users from updating anything
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime(); // Number of milliseconds after midnight Jan1 1970
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
 });
 
 app.listen(port, () => {
