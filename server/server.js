9769fcc43a1f75f36088a4597adf57dadc10acc6
Sean <seancliffdaley@gmail.com>
2018-05-14 18:47:05 +0100

    add POST /users router and token method

--- server/server.js
+++ server/server.js
@@ -72,6 +72,7 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 app.patch('/todos/:id', (req, res) => {
+    console.log(req);
     var id = req.params.id;
     // This creates a subset of the passed object, preventing users from updating anything
     var body = _.pick(req.body, ['text', 'completed']);
@@ -96,6 +97,32 @@ app.patch('/todos/:id', (req, res) => {
         res.status(400).send();
     })
 
+});
+
+// POST /users
+// app.post('/todos', (req, res) => {
+//     var todo = new Todo({
+//         text: req.body.text
+//     });
+
+//     todo.save().then((doc) => {
+//         res.send(doc);
+//     }, (e) => {
+//         res.status(400).send(e);
+//     });
+// });
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 });
 
 app.listen(port, () => {
